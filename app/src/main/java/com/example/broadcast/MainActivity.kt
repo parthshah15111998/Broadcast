8df3c03782fd062db3edc6059979245d49b8fb3d
parthshah15111998 <parths.letsnurture@gmail.com>
2022-02-07 17:36:19 +0530

    Broadcast is done

--- app/src/main/java/com/example/broadcast/MainActivity.kt
+++ app/src/main/java/com/example/broadcast/MainActivity.kt
@@ -4,39 +4,58 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Switch
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    private val TAG:String = MainActivity::class.java.simpleName
-    var txt: TextView?=null
-
+    lateinit var wifiSwitch: Switch
+    lateinit var wifiManager: WifiManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        txt=findViewById(R.id.txt)
-        val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
-        registerReceiver(batttterBroadcastReceiver,intentFilter)
+        wifiSwitch = findViewById(R.id.wifiSwitch)
+        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        /*wifiSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                wifiManager.isWifiEnabled = true
+                wifiSwitch.text = "WiFi is ON"
+            } else {
+                wifiManager.isWifiEnabled = false
+                wifiSwitch.text = "WiFi is OFF"
+            }
+        }*/
     }
-
-    private val batttterBroadcastReceiver: BroadcastReceiver = object : BroadcastReceiver(){
-        override fun onReceive(p0: Context?, p1: Intent?) {
-            if(intent?.action=="android.intent.action.BATTERY_CHANGED"){
-                val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL,-1)
-                Log.d(TAG,"onreceive: Battery level")
-                txt?.post {
-                    txt?.text=level.toString().plus("").plus("%")
+    override fun onStart() {
+        super.onStart()
+        val intentFilter = IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION)
+        registerReceiver(wifiStateReceiver, intentFilter)
+    }
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(wifiStateReceiver)
+    }
+    private val wifiStateReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE,
+                WifiManager.WIFI_STATE_UNKNOWN)) {
+                WifiManager.WIFI_STATE_ENABLED -> {
+                    wifiSwitch.isChecked = true
+                    wifiSwitch.text = "WiFi is ON"
+                    Toast.makeText(this@MainActivity, "Wifi is On", Toast.LENGTH_SHORT).show()
+                }
+                WifiManager.WIFI_STATE_DISABLED -> {
+                    wifiSwitch.isChecked = false
+                    wifiSwitch.text = "WiFi is OFF"
+                    Toast.makeText(this@MainActivity, "Wifi is Off", Toast.LENGTH_SHORT).show()
                 }
             }
         }
-    }
-    override fun onDestroy() {
-        unregisterReceiver(batttterBroadcastReceiver)
-        super.onDestroy()
     }
 
 }
